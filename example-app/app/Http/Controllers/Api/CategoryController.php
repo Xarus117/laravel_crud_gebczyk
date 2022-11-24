ab562b96523ad612aa747c90d9854e845db1a035
Xarus <cezarygebczyk117@gmail.com>
2022-11-24 19:39:23 +0100

    Último arreglo

--- example-app/app/Http/Controllers/Api/CategoryController.php
+++ example-app/app/Http/Controllers/Api/CategoryController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -34,16 +33,16 @@ class CategoryController extends Controller
             'id' => 'required'
         ]);
 
-        $res = Category::find($request)->each->delete();
-        if ($res) {
+        $datos = Category::find($request)->each->delete();
+        if ($datos) {
             $data = [
-                'status' => '1',
-                'msg' => 'Se ha borrado la categoría'
+                "status" => "1",
+                "msg" => "Se ha borrado la categoría"
             ];
         } else {
             $data = [
-                'status' => '0',
-                'msg' => 'No se ha borrado la categoría'
+                "status" => "0",
+                "msg" => "No se ha borrado la categoría"
             ];
         }
         return response()->json($data);
@@ -52,26 +51,35 @@ class CategoryController extends Controller
     public function updateCategory(Request $request)
     {
         $request->validate(['id' => 'required', 'name' => 'required', 'description' => 'required']);
-            
-        $post = Category::updateOrCreate(
-            ['name' => $request->name, 'description' => $request->description],
-            ['id' => $request->id]
-        );
-        if ($post) {
+
+        $id = $request->id;
+        $name = $request->name;
+        $description = $request->description;
+
+        $datos = Category::find($id);
+
+        // Make sure you've got the Page model
+        if ($datos) {
+            $datos->name = "$name";
+            $datos->description = "$description";
+            $datos->save();
+        }
+        if ($datos) {
             $data = [
-                'status' => '1',
-                'msg' => 'Se ha actualizado la categoría'
+                "status" => "1",
+                "msg" => "Se ha actualizado la categoría $datos->name"
             ];
         } else {
             $data = [
-                'status' => '0',
-                'msg' => 'No se ha actualizado la categoría'
+                "status" => "0",
+                "msg" => "No se ha actualizado la categoría $datos->name"
             ];
         }
         return response()->json($data);
     }
-    
-    public function readCategory(Request $request) {
+
+    public function readCategory(Request $request)
+    {
         $posts = Category::all();
 
         return response()->json($posts);
--- example-app/app/Http/Controllers/Api/ProductController.php
+++ example-app/app/Http/Controllers/Api/ProductController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -26,7 +25,7 @@ class ProductController extends Controller
         $product->category = $request->category;
         $product->price = $request->price;
         $product->stock = $request->stock;
-        
+
         $product->save();
 
         return response()->json([
@@ -42,16 +41,16 @@ class ProductController extends Controller
             'id' => 'required'
         ]);
 
-        $res = Product::find($request)->each->delete();
-        if ($res) {
+        $datos = Product::find($request)->each->delete();
+        if ($datos) {
             $data = [
-                'status' => '1',
-                'msg' => 'Se ha borrado el producto'
+                "status" => "1",
+                "msg" => "Se ha borrado el producto"
             ];
         } else {
             $data = [
-                'status' => '0',
-                'msg' => 'No se ha borrado el producto'
+                "status" => "0",
+                "msg" => "No se ha borrado el producto"
             ];
         }
         return response()->json($data);
@@ -59,32 +58,42 @@ class ProductController extends Controller
 
     public function updateProduct(Request $request)
     {
-        $request->validate(['id' => 'required', 'name' => 'required', 'description' => 'required', 'category' => 'required', 'price' => 'required', 'precio' => 'required', 'stock' => 'required']);
-        $product = new Product();
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->category = $request->category;
-        $product->precio = $request->precio;
-        $product->stock = $request->stock;
-        $post = Product::updateOrCreate(
-            ['name' => $request->name, 'description' => $request->description, 'category' => $request->category, 'price' => $request->price, 'stock' => $request->stock],
-            ['id' => $request->id]
-        );
-        if ($post) {
+        $request->validate(['id' => 'required', 'name' => 'required', 'description' => 'required', 'category' => 'required', 'price' => 'required', 'stock' => 'required']);
+
+        $id = $request->id;
+        $name = $request->name;
+        $description = $request->description;
+        $category = $request->category;
+        $price = $request->price;
+        $stock = $request->stock;
+
+        $datos = Product::find($id);
+
+        // Make sure you've got the Page model
+        if ($datos) {
+            $datos->name = "$name";
+            $datos->description = "$description";
+            $datos->category = "$category";
+            $datos->price = "$price";
+            $datos->stock = "$stock";
+            $datos->save();
+        }
+        if ($datos) {
             $data = [
-                'status' => '1',
-                'msg' => 'Se ha actualizado el producto'
+                "status" => "1",
+                "msg" => "Se ha actualizado el producto $request->name"
             ];
         } else {
             $data = [
-                'status' => '0',
-                'msg' => 'No se ha actualizado el producto'
+                "status" => "0",
+                "msg" => "No se ha actualizado el producto $request->name"
             ];
         }
         return response()->json($data);
     }
-    
-    public function readProduct(Request $request) {
+
+    public function readProduct(Request $request)
+    {
         $posts = Product::all();
 
         return response()->json($posts);
